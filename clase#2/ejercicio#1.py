84a1ead91aac6b94aab2b48f285cca658880b4c2
pabloasalazar3 <pabloasalazar3@gmail.com>
2024-08-23 16:01:05 +0000

    nuevo ejercicio

---
+++ clase#2/ejercicio#1.py
@@ -0,0 +1,20 @@
+num1=input('digite el primer numero')
+num2=input('digite el segundo numero')
+num3=input('digitee el tercer numero')
+
+if numero_uno > numero_dos and numero_uno > numero_tres:
+    if numero_dos > numero_tres:
+        print('el numero mayor es:','1',numero_uno, '2',numero_dos, "3",numero_tres)
+    else:
+        print(numero_uno, numero_tres, numero_dos)
+elif numero_dos > numero_uno and numero_dos > numero_tres:
+    if numero_uno > numero_tres:
+        print('el numero mayor es:','1',numero_dos,'3', numero_uno, '4',numero_tres)
+    else:
+        print('el numero mayor es:','1',numero_dos, '2',numero_tres, '3',numero_uno)
+else:
+    if numero_uno > numero_dos:
+        print('1',numero_tres,'2', numero_uno,'3', numero_dos)
+    else:
+        print('1',numero_tres, '2',numero_dos,'3', numero_uno)
+        
---
+++ clase#2/ejercicio#2.py
@@ -0,0 +1,6 @@
+numero=input('solicite en pantalla un numero')
+
+if numero %2==0:
+    print('el numero es par')
+else:
+    print('el numero es impar')
