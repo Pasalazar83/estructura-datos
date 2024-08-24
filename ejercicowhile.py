7a9e2a9ebf740f1296b8cb8ce9c64aa4cfee6d00
Pasalazar83 <123608556+Pasalazar83@users.noreply.github.com>
2024-08-24 15:27:45 +0000

    esto es serio

---
+++ ejercicowhile.py
@@ -0,0 +1,18 @@
+def control_ingreso(): 
+    nummax= int(input("numero maximo de personas que pueden entrar"))
+    nummax=15
+    personasingresada=0
+
+    while personasingresada <= nummax: 
+        ticket= input("muestre numero de boleto:   o salir para no dejar pasar")
+        personaingresada=+1
+    if ticket== personasingresada:
+        print("persona ingresada. total", "puede pasar")
+    else:
+     print("persona no puede pasar", nummax=personasingresada) 
+        
+        
+
+        
+        
+    
